5d674022c5524c68cce77345bc0df081022f6587
ghostbusterbob <76821810+jaysendaemon@users.noreply.github.com>
2024-11-01 00:54:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const steamUser = require('steam-user');
-const steamTotp = require('steam-totp');
-const keep_alive = require('./keep_alive.js')
+const steamUser = require("steam-user");
+const steamTotp = require("steam-totp");
+const keep_alive = require("./keep_alive.js");
 
-var username = process.env.username;
-var password = process.env.password;
-var shared_secret = process.env.shared;
+const { username, password, shared_secret } = process.env;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
-
+var games = [753, 218800]; // Enter here AppIDs of the needed games
+var status = 1; // 1 - online, 7 - invisible
 
 user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
-user.on('loggedOn', () => {
-	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-	user.setPersona(status);               
+user.logOn({
+	accountName: username,
+	password: password,
+	twoFactorCode: steamTotp.generateAuthCode(shared_secret),
+});
+user.on("loggedOn", () => {
+	if (user.steamID != null)
+		console.log(user.steamID + " - Successfully logged on");
+	user.setPersona(status);
 	user.gamesPlayed(games);
 });
-
 
 // var username2 = process.env.username2;
 // var password2 = process.env.password2;
@@ -26,11 +27,10 @@ user.on('loggedOn', () => {
 // var games2 = [730, 440, 570, 304930];  // Enter here AppIDs of the needed games
 // var status2 = 1;  // 1 - online, 7 - invisible
 
-
 // user2 = new steamUser();
 // user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
 // user2.on('loggedOn', () => {
 // 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-// 	user2.setPersona(status2);               
+// 	user2.setPersona(status2);
 // 	user2.gamesPlayed(games2);
 // });
